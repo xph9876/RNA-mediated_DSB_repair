4c638ad4f5cf1a3153d1d67217eeb0e203878874
TJ <you@example.com>
2022-06-18 14:11:41 -0400

    Update scripts for generating graphs.

--- run_9_plot_graph.ps1
+++ run_9_plot_graph.ps1
@@ -26,3 +26,22 @@ python 2_graph_processing/plot_graph.py -i libraries_4/WT_sgCD_R1_antisense -o p
 python 2_graph_processing/plot_graph.py -i libraries_4/WT_sgCD_R1_splicing -o plots/graphs/individual/png --layout_dir layouts/2DSBanti_CD -ext png --layout radial
 python 2_graph_processing/plot_graph.py -i libraries_4/WT_sgCD_R2_antisense -o plots/graphs/individual/png --layout_dir layouts/2DSBanti_CD -ext png --layout radial --reverse_complement 
 python 2_graph_processing/plot_graph.py -i libraries_4/WT_sgCD_R2_splicing -o plots/graphs/individual/png --layout_dir layouts/2DSBanti_CD -ext png --layout radial --reverse_complement
+
+python 2_graph_processing/plot_graph.py -i libraries_4/WT_sgAB_R1_sense_branch -o plots/graphs/combined/png --layout_dir layouts/2DSB_AB -ext png --layout radial
+python 2_graph_processing/plot_graph.py -i libraries_4/WT_sgAB_R1_sense_cmv -o plots/graphs/combined/png --layout_dir layouts/2DSB_AB -ext png --layout radial
+python 2_graph_processing/plot_graph.py -i libraries_4/KO_sgAB_R1_sense_branch -o plots/graphs/combined/png --layout_dir layouts/2DSB_AB -ext png --layout radial
+python 2_graph_processing/plot_graph.py -i libraries_4/KO_sgAB_R1_sense_cmv -o plots/graphs/combined/png --layout_dir layouts/2DSB_AB -ext png --layout radial
+python 2_graph_processing/plot_graph.py -i libraries_4/WT_sgAB_R2_sense_branch -o plots/graphs/combined/png --layout_dir layouts/2DSB_AB -ext png --layout radial --reverse_complement 
+python 2_graph_processing/plot_graph.py -i libraries_4/WT_sgAB_R2_sense_cmv -o plots/graphs/combined/png --layout_dir layouts/2DSB_AB -ext png --layout radial --reverse_complement 
+python 2_graph_processing/plot_graph.py -i libraries_4/KO_sgAB_R2_sense_branch -o plots/graphs/combined/png --layout_dir layouts/2DSB_AB -ext png --layout radial --reverse_complement 
+python 2_graph_processing/plot_graph.py -i libraries_4/KO_sgAB_R2_sense_cmv -o plots/graphs/combined/png --layout_dir layouts/2DSB_AB -ext png --layout radial --reverse_complement 
+python 2_graph_processing/plot_graph.py -i libraries_4/WT_sgA_R1_sense_branch -o plots/graphs/combined/png --layout_dir layouts/1DSB_A -ext png --layout radial
+python 2_graph_processing/plot_graph.py -i libraries_4/WT_sgA_R1_sense_cmv -o plots/graphs/combined/png --layout_dir layouts/1DSB_A -ext png --layout radial
+python 2_graph_processing/plot_graph.py -i libraries_4/KO_sgA_R1_sense_branch -o plots/graphs/combined/png --layout_dir layouts/1DSB_A -ext png --layout radial
+python 2_graph_processing/plot_graph.py -i libraries_4/KO_sgA_R1_sense_cmv -o plots/graphs/combined/png --layout_dir layouts/1DSB_A -ext png --layout radial
+python 2_graph_processing/plot_graph.py -i libraries_4/WT_sgB_R2_sense_branch -o plots/graphs/combined/png --layout_dir layouts/1DSB_B -ext png --layout radial
+python 2_graph_processing/plot_graph.py -i libraries_4/WT_sgB_R2_sense_cmv -o plots/graphs/combined/png --layout_dir layouts/1DSB_B -ext png --layout radial
+python 2_graph_processing/plot_graph.py -i libraries_4/KO_sgB_R2_sense_branch -o plots/graphs/combined/png --layout_dir layouts/1DSB_B -ext png --layout radial
+python 2_graph_processing/plot_graph.py -i libraries_4/KO_sgB_R2_sense_cmv -o plots/graphs/combined/png --layout_dir layouts/1DSB_B -ext png --layout radial
+python 2_graph_processing/plot_graph.py -i libraries_4/WT_sgCD_R1_antisense_splicing -o plots/graphs/combined/png --layout_dir layouts/2DSBanti_CD -ext png --layout radial
+python 2_graph_processing/plot_graph.py -i libraries_4/WT_sgCD_R2_antisense_splicing -o plots/graphs/combined/png --layout_dir layouts/2DSBanti_CD -ext png --layout radial --reverse_complement 
