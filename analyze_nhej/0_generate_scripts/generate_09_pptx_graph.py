dc9465d757c7827b75091f0f7832756517e5556d
Tejasvi Channagiri <tchannagri@mail.usf.edu>
2022-08-25 18:50:23 -0400

    Fixes to genrate graph ppt file names.

--- analyze_nhej/0_generate_scripts/generate_09_pptx_graph.py
+++ analyze_nhej/0_generate_scripts/generate_09_pptx_graph.py
@@ -6,19 +6,17 @@ import library_constants
 import generate_constants
 import generate_07_plot_graph
 
-
-def get_output_file(cell_line, dsb_type):
-  return os.path.join(
-    generate_constants.OUTPUT_DIR['pptx'],
-    cell_line,
-    dsb_type,
-  )
-
-def get_input_file(experiment_name, layout_name, format, version, ext = 'png'):
+def get_output_file(cell_line, dsb_type, version):
   version_str = '' if (version == 'versionNone') else ('_' + version)
   return os.path.join(
+    generate_constants.OUTPUT_DIR['pptx'],
+    cell_line + '_' + dsb_type + version_str + os.extsep + 'pptx',
+  )
+
+def get_input_file(experiment_name, layout_name, format, ext = 'png'):
+  return os.path.join(
     generate_07_plot_graph.get_output_dir(layout_name, format, ext),
-    experiment_name + version_str + os.path.extsep + ext
+    experiment_name + os.path.extsep + ext
   )
 
 TOTAL_WIDTH = {
@@ -89,7 +87,6 @@ if __name__ == '__main__':
                     experiment_name = info['name'],
                     layout_name = generate_constants.USE_LAYOUT,
                     format = format,
-                    version = info['version'],
                   ))
                   label_list.append(
                     library_constants.LABELS[info['guide_rna']] + '\\n' +
@@ -102,22 +99,7 @@ if __name__ == '__main__':
             arg_num_rows = '--num_rows ' + ' '.join(str(x) for x in num_rows_list)
             arg_num_cols = '--num_cols ' + ' '.join(str(x) for x in num_cols_list)
             arg_total_width = '--total_width ' + ' '.join(str(x) for x in total_width_list)
-            arg_output = '--output ' + get_output_file(cell_line, dsb_type)
+            arg_output = '--output ' + get_output_file(cell_line, dsb_type, info['version'])
             file_out.write(f"python {generate_constants.PYTHON_SCRIPTS['get_pptx']} {arg_input} {arg_output} {arg_labels} {arg_num_grids} {arg_num_rows} {arg_num_cols} {arg_total_width} {ARG_LEGEND}\n")
       log_utils.log(file_out.name)
 
-
-
-
-# for cell in ${celltypes[*]}; do
-#   # 1 DSB graphs
-#   python 3_make_pptx/make_pptx.py -i "plots/graphs/%layout%/individual/png/%%G_sgA_R1_sense.png" "plots/graphs/%layout%/individual/png/%%G_sgA_R1_branch.png" "plots/graphs/%layout%/individual/png/%%G_sgA_R1_cmv.png" "plots/graphs/%layout%/individual/png/%%G_sgB_R2_sense.png" "plots/graphs/%layout%/individual/png/%%G_sgB_R2_branch.png" "plots/graphs/%layout%/individual/png/%%G_sgB_R2_cmv.png" "plots/graphs/%layout%/combined/png/%%G_sgA_R1_sense_branch.png" "plots/graphs/%layout%/combined/png/%%G_sgA_R1_sense_cmv.png" "plots/graphs/%layout%/combined/png/%%G_sgB_R2_sense_branch.png" "plots/graphs/%layout%/combined/png/%%G_sgB_R2_sense_cmv.png" -lab "sgRNA A\nSense" "sgRNA A\nBranchΔ" "sgRNA A\npCMVΔ" "sgRNA B\nSense" "sgRNA B\nBranchΔ" "sgRNA B\npCMVΔ" "sgRNA A\nSense & BranchΔ" "sgRNA A\nSense & pCMVΔ" "sgRNA B\nSense & BranchΔ" "sgRNA B\nSense & pCMVΔ" -ng 2 -nr 2 2 -nc 3 2 -tw 1 0.95 -o "pptx/%%G_1DSB_graphs.pptx" --legends node_size freq_ratio_sense_branch freq_ratio_sense_cmv node_type edge_type
-
-#   # 2 DSB graphs
-#   python 3_make_pptx/make_pptx.py -i "plots/graphs/%layout%/individual/png/%%G_sgAB_R1_sense.png" "plots/graphs/%layout%/individual/png/%%G_sgAB_R1_branch.png" "plots/graphs/%layout%/individual/png/%%G_sgAB_R1_cmv.png" "plots/graphs/%layout%/individual/png/%%G_sgAB_R2_sense.png" "plots/graphs/%layout%/individual/png/%%G_sgAB_R2_branch.png" "plots/graphs/%layout%/individual/png/%%G_sgAB_R2_cmv.png" "plots/graphs/%layout%/combined/png/%%G_sgAB_R1_sense_branch.png" "plots/graphs/%layout%/combined/png/%%G_sgAB_R1_sense_cmv.png" "plots/graphs/%layout%/combined/png/%%G_sgAB_R2_sense_branch.png" "plots/graphs/%layout%/combined/png/%%G_sgAB_R2_sense_cmv.png" -lab "sgRNA A & B\nForward strand\nSense" "sgRNA A & B\nForward strand\nBranchΔ" "sgRNA A & B\nForward strand\npCMVΔ" "sgRNA A & B\nReverse strand\nSense" "sgRNA A & B\nReverse strand\nBranchΔ" "sgRNA A & B\nReverse strand\npCMVΔ" "sgRNA A & B\nForward strand\nSense & BranchΔ" "sgRNA A & B\nForward strand\nSense & pCMVΔ" "sgRNA A & B\nReverse strand\nSense & BranchΔ" "sgRNA A & B\nReverse strand\nSense & pCMVΔ" -ng 2 -nr 2 2 -nc 3 2 -tw 1 0.95 -o "pptx/%%G_2DSB_graphs.pptx" --legends node_size freq_ratio_sense_branch freq_ratio_sense_cmv node_type edge_type
-#   # 2 DSB antisense graphs
-#   if [ $cell = WT ]
-#   then
-#     python 3_make_pptx/make_pptx.py -i "plots/graphs/%layout%/individual/png/%%G_sgCD_R1_antisense.png" "plots/graphs/%layout%/individual/png/%%G_sgCD_R1_splicing.png" "plots/graphs/%layout%/individual/png/%%G_sgCD_R2_antisense.png" "plots/graphs/%layout%/individual/png/%%G_sgCD_R2_splicing.png" "plots/graphs/%layout%/combined/png/%%G_sgCD_R1_antisense_splicing.png" "plots/graphs/%layout%/combined/png/%%G_sgCD_R2_antisense_splicing.png" -lab "sgRNA C & D\nForward strand\nAntisense" "sgRNA C' & D\nForward strand\n5' splicingΔ" "sgRNA C & D\nReverse strand\nAntisense" "sgRNA C' & D\nReverse strand\n5' splicingΔ" "sgRNA C/C' & D\nForward strand\nAntisense & 5' splicingΔ" "sgRNA C/C' & D\nReverse strand\nAntisense & 5' splicingΔ" -ng 2 -nr 2 1 -nc 2 2 -o -tw 1 0.95 "pptx/%%G_2DSBanti_graphs.pptx" --legends node_size freq_ratio_antisense_splicing node_type edge_type
-#   fi
-# done
