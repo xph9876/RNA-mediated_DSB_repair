b4e0d658b19ba11b6a15832385453bbbacf5d47d
TJ <none@none.com>
2023-03-31 03:45:34 -0400

    IN PROGRESS. Experimenting w/ t-test and ANOVA.

--- bootstrap/bootstrap.py
+++ bootstrap/bootstrap.py
@@ -6,6 +6,10 @@ import numpy as np
 import xlsxwriter
 import seaborn as sns
 import matplotlib.pyplot as plt
+from statannotations.Annotator import Annotator
+import scipy.stats
+import statsmodels.api as sm
+from statsmodels.formula.api import ols
 
 # get the difference and relative effect size
 def calc_diff(WT_s, WT_b, KO_s, KO_b):
@@ -26,19 +30,34 @@ def resample(WT_s, WT_b, KO_s, KO_b):
         np.random.choice(KO_s, len(KO_s), replace=True),
         np.random.choice(KO_b, len(KO_b), replace=True))
 
-def draw(data, title, output):
+def draw(data, title, sig_str, output):
     # parameters
     fig, ax = plt.subplots(figsize=(6,6))
     plt.subplots_adjust(left=0.15, top=0.8, bottom=0.2, right=.9)
-    sns.barplot(x='Label', y='Freq', data=data,
-        errwidth=2, capsize=0.2, errorbar='sd', edgecolor='k', ax=ax)
+    sns.barplot(x='Label', y='Freq', data=data, edgecolor='k', ax=ax)
+    plt.errorbar(x=data['Label'], y=data['Freq'],
+            yerr=data['Err'], fmt='none', c='black', capsize=10)
+    
+    # Maybe add significance bracket
+    if sig_str is not None:
+        annotator = Annotator(ax, [('WT_s - WT_b', 'KO_s - KO_b')],\
+                              data=data, x='Label', y='Freq',\
+                              order=['WT_s - WT_b', 'KO_s - KO_b'])
+        annotator.verbose = False
+        annotator.perform_stat_test = False
+        annotator.annotate_custom_annotations([sig_str])
+
     sns.despine()
     ax.ticklabel_format(axis='y', style='sci', scilimits=(0,0),\
         useOffset=False, useMathText=True)
     plt.xticks(rotation=12)
+    low, high = plt.ylim()
+    bound = max(abs(low), abs(high))
+    plt.ylim(-bound, bound)
     plt.xlabel('')
     plt.ylabel('')
-    plt.title(title)
+    if title is not None:
+        plt.title(title)
     fig.savefig(output)
     plt.close()
 
@@ -54,6 +73,7 @@ def main():
                         help='Number of bootstrap samples. We use the form 10^p - 1 for p = 3, 4, 5, so that' +
                         ' the 2.5% and 97.% percentiles can be computed exactly.')
     parser.add_argument('-draw', default=None, help='Output dir for barplots. If not provided the barplots are not drawn.')
+    parser.add_argument('-title', action='store_true', help='If present, put a title on plots.')
     args = parser.parse_args()
 
     # read data
@@ -108,6 +128,19 @@ def main():
                 lt = len([x for x in res if x - 2 * diff <= 0])
                 pvalue = min(2 * (1 + min(gt, lt)) / (args.n + 1), 1)
 
+                # temp t-test
+                sd = np.sqrt((np.std(WT_s)**2 + np.std(WT_b)**2 + np.std(KO_s)**2 + np.std(KO_b)**2) / 4 * np.sqrt(1/4+1/4+1/4+1/4))
+                print('t-test: ' + str(2 * (1-scipy.stats.t.cdf(abs(diff / sd), df=12))))
+                print('boottest: ' + str(pvalue))
+                model = ols(
+                    'Freq ~ C(Cell) * C(Construct)',
+                    data=pd.DataFrame({'Freq': WT_s + WT_b + KO_s + KO_b,
+                    'Cell': ['WT'] * 8 + ['KO'] * 8,
+                    'Construct': ['s'] * 4 + ['b'] * 4 + ['s'] * 4 + ['b'] * 4})).fit()
+                anova_table = sm.stats.anova_lm(model, typ=2)
+                print(anova_table)
+                print('')
+
                 # write
                 ws.write(row, 0, cut)
                 ws.write(row, 1, rd)
@@ -118,21 +151,28 @@ def main():
                 # is significant?
                 if pvalue < 0.05:
                     if diff > 0:
-                        sig_str = 'WT'
+                        sig_str = '* WT'
                     else:
-                        sig_str = 'KO'
-                    ws.write(row, 5, sig_str, sig_style)
-                    draw_title += '\n' + sig_str
+                        sig_str = '* KO'
+                    style = sig_style
                 else:
-                    ws.write(row, 5, 'NS', nonsig_style)
-                    draw_title += '\nNS'
+                    sig_str = 'NS'
+                    style = nonsig_style
+                ws.write(row, 5, sig_str, style)
                 row += 1
                 if args.draw is not None:
                     draw_title = f'{cut} {rd} {name}\nP = {pvalue:.3f}\n{sig_str}'
                     draw_output = f'{cut}_{rd}_{name}.png'
-                    draw(pd.DataFrame({'Freq': WT_s + WT_b + KO_s + KO_b,
-                                       'Label': ['WT_s'] * 4 + ['WT_b'] * 4 + ['KO_s'] * 4 + ['KO_b'] * 4}),
-                        draw_title,
+                    # draw(pd.DataFrame({'Freq': WT_s + WT_b + KO_s + KO_b,
+                    #                    'Label': ['WT_s'] * 4 + ['WT_b'] * 4 + ['KO_s'] * 4 + ['KO_b'] * 4}),
+                    #     draw_title,
+                    #     args.draw + '/' + draw_output)
+                    # print([WT_s, WT_b, KO_s, KO_b])
+                    draw(pd.DataFrame({'Freq': [np.mean(WT_s) - np.mean(WT_b), np.mean(KO_s) - np.mean(KO_b)],
+                                       'Err': [np.sqrt(np.std(WT_s)**2 + np.std(WT_b)**2), np.sqrt(np.std(KO_s)**2 + np.std(KO_b)**2)],
+                                       'Label': ['WT_s - WT_b', 'KO_s - KO_b']}),
+                        draw_title if args.title else None,
+                        sig_str,
                         args.draw + '/' + draw_output)
     workbook.close()        
 
--- bootstrap/bootstrap_run.sh
+++ bootstrap/bootstrap_run.sh
@@ -1,4 +1,4 @@
-python bootstrap.py input/inverted_intron.tsv output/inverted_intron.xlsx
-python bootstrap.py input/mmej.tsv output/mmej.xlsx
-python bootstrap.py input/NHEJ.tsv output/NHEJ.xlsx
-python bootstrap.py input/R-TDR.tsv output/R-TDR.xlsx
+python bootstrap.py input/inverted_intron.tsv output/inverted_intron.xlsx -draw output/inverted_intron
+python bootstrap.py input/mmej.tsv output/mmej.xlsx -draw output/mmej
+python bootstrap.py input/NHEJ.tsv output/NHEJ.xlsx -draw output/NHEJ
+python bootstrap.py input/R-TDR.tsv output/R-TDR.xlsx -draw output/R-TDR
