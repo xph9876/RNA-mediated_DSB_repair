e1ff6f0994da4f1cddd9176984d92702d41fa452
TJ <none@none.com>
2023-06-25 14:46:15 +0530

    Add directory creation.

--- NHEJ/run_all.ps1
+++ NHEJ/run_all.ps1
@@ -1,3 +1,20 @@
+foreach (
+  $dir in @(
+    "data_0_sam",
+    "data_1_filter_nhej",
+    "data_2_combine_repeat",
+    "data_3_window",
+    "data_4_graph",
+    "data_5_histogram",
+    "data_6_precomputed_layout",
+    "data_bowtie2_build",
+    "data_fastq"
+  )
+) {
+  if (!(Test-Path $dir)) {
+    mkdir $dir
+  }
+}
 ./run_00_bowtie2_align.ps1
 ./run_01_process_nhej.ps1
 ./run_02_combine_repeat.ps1
